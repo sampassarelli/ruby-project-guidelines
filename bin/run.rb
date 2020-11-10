b8eed6e48df02807dab2719cbff8c6ed59632c6e
sampassarelli <passarelli.sam@gmail.com>
2020-11-10 12:24:37 -0600

    created CLI class in the run.rb with temp load message

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,9 @@
 require_relative '../config/environment'
 
 class CLI
-    
+    puts "WELCOME to RAK CITY MASTER OF TICKETS"
+    sleep(3)
+    puts "HERE, YOU CAN CREATE A USERNAME AND BEGIN LOOKING FOR TICKETS TO YOUR FAVORITE EVENT"
+    sleep(3)
+    puts "PLEASE CREATE THAT USERNAME"
 end
-
-
-puts "HELLO WORLD"
